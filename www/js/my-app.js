ea2b5ae41bd8fa30f2e0f2492e06c042a65b57cd
Christopher Butzen <cbutzen@gmail.com>
2017-05-08 11:53:33 -0400

    testing

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -96,8 +96,8 @@ function getArticles(){
 		data: {
 			per_page : 10, 
 			orderby : 'date',
-			sort : 'desc'
-			//exclude: postIds
+			sort : 'desc',
+			exclude: postIds
 			
 		},
 		success: function(data){
